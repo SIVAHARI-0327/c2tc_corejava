5635f6853f23b043d7a5b246ad20177513ecb236
Dell <Dell@DESKTOP-PUC9O4H>
2025-08-14 13:57:38 +0530

    hello

---
+++ c2tc_corejavaProgram/src/c2tc_corejavaProgram/firstday.java
@@ -0,0 +1,11 @@
+package c2tc_corejavaProgram;
+
+public class firstday {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+
+	}
+
+}
---
+++ c2tc_corejavaProgram/src/c2tc_corejavaProgram/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+package c2tc_corejavaProgram;
